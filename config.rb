41de693450a3f3f40b52f65d54df21596eb9bd72
lhermann <lukher6@web.de>
2015-11-18 16:30:42 -0300

    Beta 1 finished

--- config.rb
+++ config.rb
@@ -1,15 +1,18 @@
+require 'compass/import-once/activate'
 # Require any additional compass plugins here.
-add_import_path "bower_components/foundation/scss"
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+# add_import_path "/Applications/MAMP/htdocs/inuit/bower_components"
+add_import_path "bower_components"
+
+http_path = "htdocs/"
+css_dir = "htdocs/css"
+sass_dir = "htdocs/scss"
+images_dir = "htdocs/img"
+javascripts_dir = "htdocs/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+# output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -17,8 +20,9 @@ javascripts_dir = "js"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
+
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
