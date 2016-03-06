4f346e717a7a90cf96214b9e539a8b60ed2c7f16
Lukas Hermann <lukher6@web.de>
2016-03-06 11:23:43 -0300

    Fixes missing bull in exam hero meta

--- themefiles/single-exam.php
+++ themefiles/single-exam.php
@@ -1,36 +1,30 @@
-<?php
-	get_header();
-	// var_dump($post);
-?>
+<?php get_header(); ?>
 
-	<header id="page-header">
+	<header id="content-header" class="content-header page-hero page-hero--skinny shadow--receive t-header-image">
 
-		<?php get_template_part( 'templates/navigation', 'exam' ); ?>
-
-		<div class="page-hero page-hero--skinny shadow--receive t-header-image">
-			<div class="wrapper">
-
-				<div class="nl__item nl__item--exam nl__item--hero nl__item--<?= $post->status ?>">
-					<article class="nl__article">
-						<header class="nl__article__header owl--off">
-							<h1 class="nl__article__title"><span class="label label--small shadow--strong t-second-text"><?= __( 'Exam', 'gladtidings') ?></span> <span class="shadow--strong-text"><?= $post->title ?></span></h1>
-						</header>
-						<footer class="nl__article__footer shadow--strong t-comp-text">
-							<p><?= __( 'Course', 'gladtidings' ) ?>: <?= $post->parent()->link_to(); ?> <?= h_node_meta( $post ) ?></p>
-						</footer>
-					</article>
-					<div class="nl__node nl__node--big">
-						<div class="nl__node__border t-second-border"></div>
-						<div class="nl__node__inner <?= $post->status == 'publish' ? 't-main-text t-main-border' : '' ?>"></div>
-					</div>
+		<div class="wrapper">
+			<div class="nl__item nl__item--exam nl__item--hero nl__item--<?= $post->status ?>">
+				<article class="nl__article">
+					<header class="nl__article__header owl--off">
+						<h1 class="nl__article__title"><span class="label label--small shadow--strong t-second-text"><?= __( 'Exam', 'gladtidings') ?></span> <span class="shadow--strong-text"><?= $post->title ?></span></h1>
+					</header>
+					<footer class="nl__article__footer shadow--strong t-comp-text">
+						<p>
+							<?= __( 'Course', 'gladtidings' ) ?>: <?= $post->parent()->link_to(); ?>
+							<?php if( h_node_meta( $post ) ) print( '&bull; ' . h_node_meta( $post ) );  ?>
+						</p>
+					</footer>
+				</article>
+				<div class="nl__node nl__node--big">
+					<div class="nl__node__border t-second-border"></div>
+					<div class="nl__node__inner <?= $post->status == 'publish' ? 't-main-text t-main-border' : '' ?>"></div>
 				</div>
-
-			</div><!-- /.wrapper -->
-		</div>
+			</div>
+		</div><!-- /.wrapper -->
 
 	</header>
 
-	<main class="wrapper" role="main">
+	<div id="content-body" class="content-body wrapper">
 
 		<article id="post-<?= $post->ID ?>" <?php post_class( 'h-entry u-spacing--top' ); ?>>
 
@@ -47,6 +41,6 @@
 			</footer>
 		</article>
 
-	</main>
+	</div><!-- /#content-body /.wrapper -->
 
 <?php get_footer(); ?>
