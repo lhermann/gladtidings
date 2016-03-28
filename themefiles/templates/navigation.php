d4cc83d2c318158a4e9b7b9a088f935f0a17a51d
Lukas Hermann <lukher6@web.de>
2016-03-27 21:46:20 -0300

    Fixed user menu code duplication

--- themefiles/templates/navigation.php
+++ themefiles/templates/navigation.php
@@ -26,15 +26,7 @@
 				</a>
 				<div id="user-menu" class="top-bar__user-menu">
 					<ul class="user-menu user-menu--mouse">
-						<li class="user-menu__item user-menu__identity">
-							<?= get_avatar( $user->ID, 60 ) ?>
-							<div class="user-menu__name"><?= $user->first_name . ' ' . $user->last_name ?></div>
-							<div class="user-menu__role"><?= __( 'Student', 'gladtidings' ) ?></div>
-						</li>
-						<li class="user-menu__item"><?= $user->link_to( 'dashboard' ) ?></li>
-						<li class="user-menu__item"><?= $user->link_to( 'settings' ) ?></li>
-						<li class="user-menu__item"><?= $user->link_to( 'contact' ) ?></li>
-						<li class="user-menu__item"><?php wp_loginout( $_SERVER['REQUEST_URI'] ); ?></li>
+						<?php get_template_part( 'templates/user-menu' ); ?>
 					</ul>
 				</div>
 			</li>
