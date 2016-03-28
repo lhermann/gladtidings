c34b899a02bb6d4eab11defab8edefca40bfa636
Lukas Hermann <lukher6@web.de>
2016-03-27 21:41:50 -0300

    Message styling

--- themefiles/archive-message.php
+++ themefiles/archive-message.php
@@ -19,7 +19,36 @@
 
 			--><div class="layout__item u-2/3-lap-and-up">
 
-				<h2>List of Messages</h2>
+				<h3>List of Messages</h3>
+				<hr class="ui">
+				<li class="message message--compact owl--offall">
+					<a href="#" class="message__link a--unchanged">
+						<p class="message__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, odit eos corporis rem ab. Obcaecati porro veniam consequatur nihil, illo quidem. Dolores tenetur dignissimos numquam asperiores veniam doloribus quidem necessitatibus consectetur dolorem.</p>
+						<footer class="message__footer">
+							<div class="message__id">#171</div>
+							<div class="message__meta">
+								<span class="badge badge--success">1</span> Answer  &bull; <strong>Unread Answer</strong>
+							</div>
+							<div class="message__controlls">
+								<span class="fi fi-arrows-expand"></span>
+							</div>
+						</footer>
+					</a>
+				</li>
+				<li class="message message--compact owl--offall">
+					<a href="#" class="message__link a--unchanged">
+						<p class="message__content">Laudantium ipsam cupiditate porro est cumque iste quidem. Dolore adipisci illum, voluptates. Ullam beatae corporis quis.</p>
+						<footer class="message__footer">
+							<div class="message__id">#102</div>
+							<div class="message__meta">
+								<span class="badge">3</span> Answers
+							</div>
+							<div class="message__controlls">
+								<span class="fi fi-arrows-expand"></span>
+							</div>
+						</footer>
+					</a>
+				</li>
 
 			</div>
 
